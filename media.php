ec628c389a13826e830ff9db1412bb57ce8b4b33
Nick314159 <nsteckley314@gmail.com>
2018-08-09 14:16:51 -0700

    Made everything arial

--- media.php
+++ media.php
@@ -19,52 +19,49 @@
     <article class="post">
 
       <h2> Media</h2>
-    <p class="c0 c19"><span class="c8 c6"></span></p>
-    <p class="c0"><span class="c17 c2">Media Coverage Related to Cuttler et al. (2018) A Naturalistic Examination of the Perceived Effects of Cannabis on Negative Affect.</span></p>
-    <p class="c3"><span class="c8 c6">Forbes:</span></p>
-    <p class="c3"><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://www.forbes.com/sites/daviddisalvo/2018/04/24/how-do-marijuana-strains-and-doses-affect-depression-and-anxiety-a-new-study-looks-for-answers/%2330b5d6bd2a1f&amp;sa=D&amp;ust=1533848252615000">https://www.forbes.com/sites/daviddisalvo/2018/04/24/how-do-marijuana-strains-and-doses-affect-depression-and-anxiety-a-new-study-looks-for-answers/#30b5d6bd2a1f<br></a></span></p>
-    <p class="c3"><span class="c8 c6">Medical News Today:</span></p>
-    <p class="c3"><span class="c2">&nbsp;</span><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://www.medicalnewstoday.com/articles/321589.php&amp;sa=D&amp;ust=1533848252616000">https://www.medicalnewstoday.com/articles/321589.php</a></span></p>
-    <p class="c3"><span class="c2"><br></span><span class="c6">Medical Press</span><span class="c2">:</span></p>
-    <p class="c3"><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://medicalxpress.com/news/2018-04-scientific-guidelines-cannabis-stress-anxiety.html&amp;sa=D&amp;ust=1533848252616000">https://medicalxpress.com/news/2018-04-scientific-guidelines-cannabis-stress-anxiety.html</a></span></p>
-    <p class="c3 c19"><span class="c17 c2"></span></p>
-    <p class="c3"><span class="c8 c6">Medical Daily:</span></p>
-    <p class="c3"><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://www.medicaldaily.com/marijuana-can-improve-emotional-well-being-if-used-properly-423689&amp;sa=D&amp;ust=1533848252617000">https://www.medicaldaily.com/marijuana-can-improve-emotional-well-being-if-used-properly-423689</a></span></p>
-    <p class="c3 c19"><span class="c17 c2"></span></p>
-    <p class="c3"><span class="c8 c6">Science Daily</span></p>
-    <p class="c3"><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://www.sciencedaily.com/releases/2018/04/180419100110.htm&amp;sa=D&amp;ust=1533848252617000">https://www.sciencedaily.com/releases/2018/04/180419100110.htm</a></span></p>
-    <p class="c0 c19"><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://www.sciencedaily.com/releases/2018/04/180419100110.htm&amp;sa=D&amp;ust=1533848252618000"></a></span></p>
-
-  </br>
-    <p class="c0"><span class="c2"><br></span><span class="c17 c2">Media Coverage Related to Cuttler et al. (2017) Blunted Stress Reactivity in Chronic Cannabis Users</span></p>
-    <p class="c0"><span class="c8 c6">Forbes:</span></p>
-    <p class="c0"><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://www.forbes.com/sites/daviddisalvo/2017/08/29/why-marijuana-compounds-could-eventually-replace-anti-anxiety-meds/%234454f63191b3&amp;sa=D&amp;ust=1533848252618000">https://www.forbes.com/sites/daviddisalvo/2017/08/29/why-marijuana-compounds-could-eventually-replace-anti-anxiety-meds/#4454f63191b3</a></span></p>
-    <p class="c0"><span class="c6"><br></span><span class="c8 c6">MSN.com:</span></p>
-    <p class="c0"><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://www.msn.com/en-us/health/medical/why-marijuana-compounds-could-eventually-replace-anti-anxiety-meds/ar-AAqW2k9?srcref%3Drss&amp;sa=D&amp;ust=1533848252619000">https://www.msn.com/en-us/health/medical/why-marijuana-compounds-could-eventually-replace-anti-anxiety-meds/ar-AAqW2k9?srcref=rss</a></span></p>
-    <p class="c0"><span class="c6"><br></span><span class="c8 c6">Washington Post:</span></p>
-    <p class="c0"><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://www.washingtonpost.com/news/wonk/wp/2017/07/31/your-stoner-friend-really-is-a-lot-more-relaxed-than-you-scientists-confirm/?utm_term%3D.700e72b4b9b0&amp;sa=D&amp;ust=1533848252620000">https://www.washingtonpost.com/news/wonk/wp/2017/07/31/your-stoner-friend-really-is-a-lot-more-relaxed-than-you-scientists-confirm/?utm_term=.700e72b4b9b0</a></span></p>
-    <p class="c0"><span class="c8 c6">Leafly:</span></p>
-    <p class="c0"><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://www.leafly.com/news/health/could-cannabis-eventually-replace-anti-anxiety-medications&amp;sa=D&amp;ust=1533848252620000">https://www.leafly.com/news/health/could-cannabis-eventually-replace-anti-anxiety-medications</a></span></p>
-    <p class="c0 c19"><span class="c8 c6"></span></p>
-    <p class="c0"><span class="c8 c6">Science Daily:</span></p>
-    <p class="c0"><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://www.sciencedaily.com/releases/2017/07/170731090828.htm&amp;sa=D&amp;ust=1533848252621000">https://www.sciencedaily.com/releases/2017/07/170731090828.htm</a></span></p>
-    <p class="c0 c19"><span class="c8 c6"></span></p>
-    <p class="c0"><span class="c8 c6">MedicalResearch.com:</span></p>
-    <p class="c0"><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://medicalresearch.com/author-interviews/chronic-cannabis-users-have-blunted-response-to-stress/36236/&amp;sa=D&amp;ust=1533848252622000">https://medicalresearch.com/author-interviews/chronic-cannabis-users-have-blunted-response-to-stress/36236/</a></span></p>
-    <p class="c0 c19"><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://medicalresearch.com/author-interviews/chronic-cannabis-users-have-blunted-response-to-stress/36236/&amp;sa=D&amp;ust=1533848252622000"></a></span></p>
-  </br>
-    <p class="c0"><span class="c17 c2">Media Coverage Related to Cuttler et al. (2011) Everyday Life Memory Deficits in Pregnant Women</span></p>
-    <p class="c0"><span class="c8 c6">APA Monitor on Psychology:</span></p>
-    <p class="c0"><span class="c1"><a class="c14" href="https://www.google.com/url?q=http://gradpsych.apags.org/monitor/2008/09/pregnancy.aspx&amp;sa=D&amp;ust=1533848252623000">http://gradpsych.apags.org/monitor/2008/09/pregnancy.aspx</a></span></p>
-    <p class="c0 c19"><span class="c17 c2"></span></p>
-    <p class="c0"><span class="c8 c6">CTV News:</span></p>
-    <p class="c0"><span class="c8 c6">&ldquo;Baby Brain&rdquo;</span></p>
-    <p class="c0 c19"><span class="c17 c2"></span></p>
-    <p class="c0"><span class="c8 c6">Mother Nature Network:</span></p>
-    <p class="c0"><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://www.mnn.com/family/family-activities/stories/is-momnesia-for-real&amp;sa=D&amp;ust=1533848252624000">https://www.mnn.com/family/family-activities/stories/is-momnesia-for-real</a></span></p>
-    <p class="c0 c19"><span class="c17 c2"></span></p>
-    <p class="c0"><span class="c8 c6">NewParent:</span></p>
-    <p class="c0"><span class="c1"><a class="c14" href="https://www.google.com/url?q=http://newparent.com/toddler/does-pregnancy-effect-your-memory&amp;sa=D&amp;ust=1533848252624000">http://newparent.com/toddler/does-pregnancy-effect-your-memory</a></span></p>
+      <p class="c7"><span class="c9 c5"></span></p>
+      <p class="c8"><span class="c9 c2">Media Coverage Related to Cuttler et al. (2018) A Naturalistic Examination of the Perceived Effects of Cannabis on Negative Affect.</span></p>
+      <p class="c6"><span class="c9 c5">Forbes:</span></p>
+      <p class="c6"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://www.forbes.com/sites/daviddisalvo/2018/04/24/how-do-marijuana-strains-and-doses-affect-depression-and-anxiety-a-new-study-looks-for-answers/%2330b5d6bd2a1f&amp;sa=D&amp;ust=1533852749375000">https://www.forbes.com/sites/daviddisalvo/2018/04/24/how-do-marijuana-strains-and-doses-affect-depression-and-anxiety-a-new-study-looks-for-answers/#30b5d6bd2a1f<br></a></span></p>
+      <p class="c6"><span class="c9 c5">Medical News Today:</span></p>
+      <p class="c6"><span class="c2">&nbsp;</span><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://www.medicalnewstoday.com/articles/321589.php&amp;sa=D&amp;ust=1533852749376000">https://www.medicalnewstoday.com/articles/321589.php</a></span></p>
+      <p class="c6"><span class="c2"><br></span><span class="c5">Medical Press</span><span class="c2">:</span></p>
+      <p class="c6"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://medicalxpress.com/news/2018-04-scientific-guidelines-cannabis-stress-anxiety.html&amp;sa=D&amp;ust=1533852749376000">https://medicalxpress.com/news/2018-04-scientific-guidelines-cannabis-stress-anxiety.html</a></span></p>
+      <p class="c1"><span class="c9 c2"></span></p>
+      <p class="c6"><span class="c9 c5">Medical Daily:</span></p>
+      <p class="c6"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://www.medicaldaily.com/marijuana-can-improve-emotional-well-being-if-used-properly-423689&amp;sa=D&amp;ust=1533852749377000">https://www.medicaldaily.com/marijuana-can-improve-emotional-well-being-if-used-properly-423689</a></span></p>
+      <p class="c1"><span class="c9 c2"></span></p>
+      <p class="c6"><span class="c9 c5">Science DailyL</span></p>
+      <p class="c6"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://www.sciencedaily.com/releases/2018/04/180419100110.htm&amp;sa=D&amp;ust=1533852749377000">https://www.sciencedaily.com/releases/2018/04/180419100110.htm</a></span></p>
+      <p class="c7"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://www.sciencedaily.com/releases/2018/04/180419100110.htm&amp;sa=D&amp;ust=1533852749378000"></a></span></p>
+  </br>    <p class="c8"><span class="c2"><br></span><span class="c9 c2">Media Coverage Related to Cuttler et al. (2017) Blunted Stress Reactivity in Chronic Cannabis Users</span></p>
+      <p class="c8"><span class="c9 c5">Forbes:</span></p>
+      <p class="c8"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://www.forbes.com/sites/daviddisalvo/2017/08/29/why-marijuana-compounds-could-eventually-replace-anti-anxiety-meds/%234454f63191b3&amp;sa=D&amp;ust=1533852749379000">https://www.forbes.com/sites/daviddisalvo/2017/08/29/why-marijuana-compounds-could-eventually-replace-anti-anxiety-meds/#4454f63191b3</a></span></p>
+      <p class="c8"><span class="c5"><br></span><span class="c9 c5">MSN.com:</span></p>
+      <p class="c8"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://www.msn.com/en-us/health/medical/why-marijuana-compounds-could-eventually-replace-anti-anxiety-meds/ar-AAqW2k9?srcref%3Drss&amp;sa=D&amp;ust=1533852749379000">https://www.msn.com/en-us/health/medical/why-marijuana-compounds-could-eventually-replace-anti-anxiety-meds/ar-AAqW2k9?srcref=rss</a></span></p>
+      <p class="c8"><span class="c5"><br></span><span class="c9 c5">Washington Post:</span></p>
+      <p class="c8"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://www.washingtonpost.com/news/wonk/wp/2017/07/31/your-stoner-friend-really-is-a-lot-more-relaxed-than-you-scientists-confirm/?utm_term%3D.700e72b4b9b0&amp;sa=D&amp;ust=1533852749380000">https://www.washingtonpost.com/news/wonk/wp/2017/07/31/your-stoner-friend-really-is-a-lot-more-relaxed-than-you-scientists-confirm/?utm_term=.700e72b4b9b0</a></span></p>
+      <p class="c8"><span class="c9 c5">Leafly:</span></p>
+      <p class="c8"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://www.leafly.com/news/health/could-cannabis-eventually-replace-anti-anxiety-medications&amp;sa=D&amp;ust=1533852749381000">https://www.leafly.com/news/health/could-cannabis-eventually-replace-anti-anxiety-medications</a></span></p>
+      <p class="c7"><span class="c9 c5"></span></p>
+      <p class="c8"><span class="c9 c5">Science Daily:</span></p>
+      <p class="c8"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://www.sciencedaily.com/releases/2017/07/170731090828.htm&amp;sa=D&amp;ust=1533852749381000">https://www.sciencedaily.com/releases/2017/07/170731090828.htm</a></span></p>
+      <p class="c7"><span class="c9 c5"></span></p>
+      <p class="c8"><span class="c9 c5">MedicalResearch.com:</span></p>
+      <p class="c8"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://medicalresearch.com/author-interviews/chronic-cannabis-users-have-blunted-response-to-stress/36236/&amp;sa=D&amp;ust=1533852749382000">https://medicalresearch.com/author-interviews/chronic-cannabis-users-have-blunted-response-to-stress/36236/</a></span></p>
+      <p class="c7"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://medicalresearch.com/author-interviews/chronic-cannabis-users-have-blunted-response-to-stress/36236/&amp;sa=D&amp;ust=1533852749382000"></a></span></p>
+    </br>  <p class="c8"><span class="c9 c2">Media Coverage Related to Cuttler et al. (2011) Everyday Life Memory Deficits in Pregnant Women</span></p>
+      <p class="c8"><span class="c9 c5">APA Monitor on Psychology:</span></p>
+      <p class="c8"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=http://gradpsych.apags.org/monitor/2008/09/pregnancy.aspx&amp;sa=D&amp;ust=1533852749383000">http://gradpsych.apags.org/monitor/2008/09/pregnancy.aspx</a></span></p>
+      <p class="c7"><span class="c9 c2"></span></p>
+      <p class="c8"><span class="c9 c5">CTV News:</span></p>
+      <p class="c8"><span class="c9 c5">&ldquo;Baby Brain&rdquo;</span></p>
+      <p class="c7"><span class="c9 c2"></span></p>
+      <p class="c8"><span class="c9 c5">Mother Nature Network:</span></p>
+      <p class="c8"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://www.mnn.com/family/family-activities/stories/is-momnesia-for-real&amp;sa=D&amp;ust=1533852749383000">https://www.mnn.com/family/family-activities/stories/is-momnesia-for-real</a></span></p>
+      <p class="c7"><span class="c9 c2"></span></p>
+      <p class="c8"><span class="c9 c5">NewParent:</span></p>
+      <p class="c8"><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=http://newparent.com/toddler/does-pregnancy-effect-your-memory&amp;sa=D&amp;ust=1533852749384000">http://newparent.com/toddler/does-pregnancy-effect-your-memory</a></span></p>
 
     </article>
 </div>
--- teaching.php
+++ teaching.php
@@ -29,9 +29,10 @@
 </div>
 </br>
 <h2> Open Educational Resources</h2>
-<ol class="c18 lst-kix_oa8wlvb1tl91-0 start" start="1">
-    <li class="c11 c30"><span class="c6 c12">Price, P. C., Jhangiani, R. S., Chiang, I. A., Leighton, D. C., &amp; </span><span class="c2 c12">Cuttler, C. </span><span class="c6 c12">(2017). Research methods in psychology: 3</span><span class="c6 c23 c12">rd</span><span class="c6 c12">&nbsp;American edition. Pullman, WA: Open Text WSU. Retrieve at: https:</span><span class="c1"><a class="c14" href="https://www.google.com/url?q=http://opentext.wsu.edu/carriecuttler/&amp;sa=D&amp;ust=1533848252625000">opentext.wsu.edu/carriecuttler/</a></span></li>
-    <li class="c11 c30"><span class="c6 c12">Bridley, A. &amp; Daffin, L. W. Jr. (2018). Abnormal Psychology: 1</span><span class="c6 c23 c12">st</span><span class="c6 c12">&nbsp;edition. </span><span class="c2 c12">C. Cuttler</span><span class="c6 c12">&nbsp;(Ed.). Pullman, WA: Open Text WSU. Retrieve at: </span><span class="c1"><a class="c14" href="https://www.google.com/url?q=https://opentext.wsu.edu/abnormalpsychology/&amp;sa=D&amp;ust=1533848252626000">https://opentext.wsu.edu/abnormalpsychology/</a></span></li>
+<p class="c7"><span class="c9 c21"></span></p>
+<ol class="c12 lst-kix_oa8wlvb1tl91-0 start" start="1">
+    <li class="c24 c33"><span class="c5 c17">Price, P. C., Jhangiani, R. S., Chiang, I. A., Leighton, D. C., &amp; </span><span class="c2 c17">Cuttler, C. </span><span class="c5 c17">(2017). Research methods in psychology: 3</span><span class="c5 c17 c26">rd</span><span class="c5 c17">&nbsp;American edition. Pullman, WA: Open Text WSU. Retrieve at: https:</span><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=http://opentext.wsu.edu/carriecuttler/&amp;sa=D&amp;ust=1533852749385000">opentext.wsu.edu/carriecuttler/</a></span></li>
+    <li class="c24 c33"><span class="c5 c17">Bridley, A. &amp; Daffin, L. W. Jr. (2018). Abnormal Psychology: 1</span><span class="c5 c17 c26">st</span><span class="c5 c17">&nbsp;edition. </span><span class="c2 c17">C. Cuttler</span><span class="c5 c17">&nbsp;(Ed.). Pullman, WA: Open Text WSU. Retrieve at: </span><span class="c14 c5 c15"><a class="c10" href="https://www.google.com/url?q=https://opentext.wsu.edu/abnormalpsychology/&amp;sa=D&amp;ust=1533852749385000">https://opentext.wsu.edu/abnormalpsychology/</a></span></li>
 </ol>
     </article>
 </div>
