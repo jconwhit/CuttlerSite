e154ce351cb0280ffbc5ea7aec62528be879513b
Nick314159 <nsteckley314@gmail.com>
2018-08-13 08:01:52 -0700

    Made requested chagnes to media page

--- media.php
+++ media.php
@@ -25,6 +25,7 @@
                     <p class="c21 c12"><span class="c1"></span></p>
                     <p class="c21"><span class="c1">Forbes:</span></p>
                     <p class="c21"><span class="c10 c3 c8"><a class="c23" href="https://www.google.com/url?q=https://www.forbes.com/sites/daviddisalvo/2018/04/24/how-do-marijuana-strains-and-doses-affect-depression-and-anxiety-a-new-study-looks-for-answers/%2330b5d6bd2a1f&amp;sa=D&amp;ust=1533916575815000">https://www.forbes.com/sites/daviddisalvo/2018/04/24/how-do-marijuana-strains-and-doses-affect-depression-and-anxiety-a-new-study-looks-for-answers/#30b5d6bd2a1f<br></a></span></p>
+                    <p class="c21 c12"><span class="c6 c2 c8"></span></p>
                     <p class="c21"><span class="c1">Medical News Today:</span></p>
                     <p class="c21"><span class="c6">&nbsp;</span><span class="c10 c3 c8"><a class="c23" href="https://www.google.com/url?q=https://www.medicalnewstoday.com/articles/321589.php&amp;sa=D&amp;ust=1533916575816000">https://www.medicalnewstoday.com/articles/321589.php</a></span></p>
                     <p class="c21"><span class="c6"><br></span><span class="c3">Medical Press</span><span class="c6">:</span></p>
@@ -33,11 +34,20 @@
                     <p class="c21"><span class="c1">Medical Daily:</span></p>
                     <p class="c21"><span class="c10 c3 c8"><a class="c23" href="https://www.google.com/url?q=https://www.medicaldaily.com/marijuana-can-improve-emotional-well-being-if-used-properly-423689&amp;sa=D&amp;ust=1533916575817000">https://www.medicaldaily.com/marijuana-can-improve-emotional-well-being-if-used-properly-423689</a></span></p>
                     <p class="c21 c12"><span class="c6 c2 c8"></span></p>
-                    <p class="c21"><span class="c1">Science DailyL</span></p>
-                    <p class="c21"><span class="c10 c3 c8"><a class="c23" href="https://www.google.com/url?q=https://www.sciencedaily.com/releases/2018/04/180419100110.htm&amp;sa=D&amp;ust=1533916575818000">https://www.sciencedaily.com/releases/2018/04/180419100110.htm</a></span></p>
-                    <p class="c4 c12"><span class="c10 c3 c8"><a class="c23" href="https://www.google.com/url?q=https://www.sciencedaily.com/releases/2018/04/180419100110.htm&amp;sa=D&amp;ust=1533916575818000"></a></span></p>
+                    <p class="c21"><span class="c1">Science Daily:</span></p>
+                    <p class="c21"><span class="c10 c3 c8"><a class="c23" href="https://www.sciencedaily.com/releases/2018/04/180419100110.htm">https://www.sciencedaily.com/releases/2018/04/180419100110.htm</a></span></p>
+                    <p class="c4 c12"><span class="c6 c2 c8"></span></p>
+                    <p class="c21"><span class="c1">The Sun (UK):</span></p>
+                    <p class="c21"><span class="c10 c3 c8"><a class="c23" href="https://www.thesun.co.uk/news/6137739/cannabis-ease-depression-stress-anxiety/">https://www.thesun.co.uk/news/6137739/cannabis-ease-depression-stress-anxiety/</a></span></p>
+                    <p class="c4 c12"><span class="c6 c2 c8"></span></p>
+                    <p class="c21"><span class="c1">ArsTechnia:</span></p>
+                    <p class="c21"><span class="c10 c3 c8"><a class="c23" href="https://arstechnica.com/science/2018/04/here-are-the-types-of-marijuana-best-for-stress-and-anxiety-according-to-users/">https://arstechnica.com/science/2018/04/here-are-the-types-of-marijuana-best-for-stress-and-anxiety-according-to-users/</a></span></p>
+                    <p class="c4 c12"><span class="c6 c2 c8"></span></p>
+                    <p class="c21"><span class="c1">Daily Mail:</span></p>
+                    <p class="c21"><span class="c10 c3 c8"><a class="c23" href="http://www.dailymail.co.uk/health/article-5655621/Just-one-puff-cannabis-reduce-depression.html">http://www.dailymail.co.uk/health/article-5655621/Just-one-puff-cannabis-reduce-depression.html</a></span></p>
                     <p class="c4 c12"><span class="c6 c2 c8"></span></p>
                     <p class="c4 c12"><span class="c6 c2 c8"></span></p>
+
                     <p class="c4"><span class="c6 c2 c8">Media Coverage Related to Cuttler et al. (2017) Blunted Stress Reactivity in Chronic Cannabis Users</span></p>
                     <p class="c4 c12"><span class="c1"></span></p>
                     <p class="c4"><span class="c1">Forbes:</span></p>
@@ -46,6 +56,7 @@
                     <p class="c4"><span class="c3 c8 c10"><a class="c23" href="https://www.google.com/url?q=https://www.msn.com/en-us/health/medical/why-marijuana-compounds-could-eventually-replace-anti-anxiety-meds/ar-AAqW2k9?srcref%3Drss&amp;sa=D&amp;ust=1533916575820000">https://www.msn.com/en-us/health/medical/why-marijuana-compounds-could-eventually-replace-anti-anxiety-meds/ar-AAqW2k9?srcref=rss</a></span></p>
                     <p class="c4"><span class="c3"><br></span><span class="c1">Washington Post:</span></p>
                     <p class="c4"><span class="c10 c3 c8"><a class="c23" href="https://www.google.com/url?q=https://www.washingtonpost.com/news/wonk/wp/2017/07/31/your-stoner-friend-really-is-a-lot-more-relaxed-than-you-scientists-confirm/?utm_term%3D.700e72b4b9b0&amp;sa=D&amp;ust=1533916575821000">https://www.washingtonpost.com/news/wonk/wp/2017/07/31/your-stoner-friend-really-is-a-lot-more-relaxed-than-you-scientists-confirm/?utm_term=.700e72b4b9b0</a></span></p>
+                    <p class="c21 c12"><span class="c6 c2 c8"></span></p>
                     <p class="c4"><span class="c1">Leafly:</span></p>
                     <p class="c4"><span class="c10 c3 c8"><a class="c23" href="https://www.google.com/url?q=https://www.leafly.com/news/health/could-cannabis-eventually-replace-anti-anxiety-medications&amp;sa=D&amp;ust=1533916575821000">https://www.leafly.com/news/health/could-cannabis-eventually-replace-anti-anxiety-medications</a></span></p>
                     <p class="c4 c12"><span class="c1"></span></p>
@@ -55,14 +66,28 @@
                     <p class="c4"><span class="c1">MedicalResearch.com:</span></p>
                     <p class="c4"><span class="c10 c3 c8"><a class="c23" href="https://www.google.com/url?q=https://medicalresearch.com/author-interviews/chronic-cannabis-users-have-blunted-response-to-stress/36236/&amp;sa=D&amp;ust=1533916575822000">https://medicalresearch.com/author-interviews/chronic-cannabis-users-have-blunted-response-to-stress/36236/</a></span></p>
                     <p class="c4"><span class="c6 c2 c8">&nbsp;</span></p>
+                    <p class="c4"><span class="c1">Newsweek:</span></p>
+                    <p class="c4"><span class="c10 c3 c8"><a class="c23" href="http://www.newsweek.com/marijuana-stress-anxiety-study-legal-644918">http://www.newsweek.com/marijuana-stress-anxiety-study-legal-644918</a></span></p>
+                    <p class="c4"><span class="c6 c2 c8">&nbsp;</span></p>
                     <p class="c4 c12"><span class="c6 c2 c8"></span></p>
+
+
+                    <p class="c4"><span class="c6 c2 c8">Media Coverage Related to LaFrance & Cuttler (2017) Inspired by Mary Jane: Mechanisms Underlying Enhanced Creativity in Cannabis Users</span></p>
+                    <p class="c4 c12"><span class="c1"></span></p>
+                    <p class="c4"><span class="c1">The Daily Beat:</span></p>
+                    <p class="c4"><span class="c10 c3 c8"><a class="c23" href="http://www.btrtoday.com/listen/thedailybeat/week-of-112017-7/">http://www.btrtoday.com/listen/thedailybeat/week-of-112017-7/</a></span></p>
+                    <p class="c4 c12"><span class="c6 c2 c8"></span></p>
+                    <p class="c4"><span class="c1">PsyPost:</span></p>
+                    <p class="c4"><span class="c10 c3 c8"><a class="c23" href="http://www.psypost.org/2017/11/study-marijuana-users-creative-might-not-marijuana-50178">http://www.psypost.org/2017/11/study-marijuana-users-creative-might-not-marijuana-50178</a></span></p>
+                    <p class="c4 c12"><span class="c6 c2 c8"></span></p>
+                    <p class="c4"><span class="c6 c2 c8">&nbsp;</span></p>
+                    <p class="c4 c12"><span class="c6 c2 c8"></span></p>
+
+
                     <p class="c4"><span class="c6 c2 c8">Media Coverage Related to Cuttler et al. (2011) Everyday Life Memory Deficits in Pregnant Women</span></p>
                     <p class="c4 c12"><span class="c1"></span></p>
                     <p class="c4"><span class="c1">APA Monitor on Psychology:</span></p>
                     <p class="c4"><span class="c10 c3 c8"><a class="c23" href="https://www.google.com/url?q=http://gradpsych.apags.org/monitor/2008/09/pregnancy.aspx&amp;sa=D&amp;ust=1533916575823000">http://gradpsych.apags.org/monitor/2008/09/pregnancy.aspx</a></span></p>
-                    <p class="c4 c12"><span class="c6 c2 c8"></span></p>
-                    <p class="c4"><span class="c1">CTV News:</span></p>
-                    <p class="c4"><span class="c1">&ldquo;Baby Brain&rdquo;</span></p>
                     <p class="c4 c12"><span class="c6 c2 c8"></span></p>
                     <p class="c4"><span class="c1">Mother Nature Network:</span></p>
                     <p class="c4"><span class="c10 c3 c8"><a class="c23" href="https://www.google.com/url?q=https://www.mnn.com/family/family-activities/stories/is-momnesia-for-real&amp;sa=D&amp;ust=1533916575824000">https://www.mnn.com/family/family-activities/stories/is-momnesia-for-real</a></span></p>
